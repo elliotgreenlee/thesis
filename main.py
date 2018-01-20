22c2848095510a91dce6dafb6f9a3a81fbd4fd56
Elliot Greenlee <egreenle@vols.utk.edu>
2018-01-20 12:22:22 -0500

    add main

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Elliot Greenlee
+Date Started: 2018-01-18
+University of Tennessee, Knoxville: WIND
+"""
+
+
+def main():
+    print("Hello World")
+    return
+
+if __name__ == "__main__":
+    main()
