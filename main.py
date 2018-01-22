93896053180d0551d14c24de15253eeb20e73941
Elliot Greenlee <egreenle@vols.utk.edu>
2018-01-22 11:46:23 -0500

    add linear main program

--- main.py
+++ main.py
@@ -6,27 +6,54 @@ University of Tennessee, Knoxville: WIND
 
 import numpy as np
 import cv2
+from target_detector import detect_targets
+from target import Target
+from tracker import Tracker
+from directionality import aggregator, direction
+from visualize import visualize
 
 
-def main():
-    cap = cv2.VideoCapture(0)
-    #cap = cv2.VideoCapture("filename")
+def main():  # TODO: pass variable to main that switches between recording and reading
 
-    while (True):
-        # Capture frame-by-frame
-        ret, frame = cap.read()
+    # Start obtaining nuclear data
+    # TODO: Setup pipe?
+    # TODO: open file
 
-        # Our operations on the frame come here
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.flip(gray, 1)
+    # Start obtaining video data
+    video_capture = cv2.VideoCapture(0)
+    #video_capture = cv2.VideoCapture("filename")
 
-        # Display the resulting frame
-        cv2.imshow('frame', gray)
+    # Capture the first frame
+    capturing, frame = video_capture.read()
+
+    # Detect objects in the frame
+    targets = detect_targets(frame)
+
+    # Initialize the tracker
+    tracker = Tracker(frame, targets)
+
+    while capturing:
+        # Extract counts from file for the last frame
+        counts = aggregator()
+
+        # Find angle from directionality
+        angle = direction(counts)
+
+        # Capture the current frame
+        capturing, frame = video_capture.read()
+
+        # Update the target locations for the current frame
+        targets = tracker.track(frame, targets)
+
+        # Visualize the resulting frame
+        visualize(frame, targets, angle)
+
+        # Press q to stop
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # When everything done, release the capture
-    cap.release()
+    # Close out application
+    video_capture.release()
     cv2.destroyAllWindows()
     return
 
