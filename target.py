3f4b7d2b60f6a8e5819d04756413a632dc559fde
Elliot Greenlee <egreenle@vols.utk.edu>
2018-01-22 11:48:17 -0500

    add basic target class

---
+++ target.py
@@ -0,0 +1,13 @@
+"""
+Elliot Greenlee
+Date Started: 2018-01-22
+University of Tennessee, Knoxville: WIND
+"""
+
+
+class Target:
+    def __init__(self, rectangle, object_type):
+        self.object_type = object_type
+        self.rectangle = rectangle
+        return
+
