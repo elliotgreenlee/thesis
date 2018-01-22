58960301113388e9da3846141c9a962f03315178
Elliot Greenlee <egreenle@vols.utk.edu>
2018-01-22 11:49:00 -0500

    add object tracker stub

---
+++ tracker.py
@@ -0,0 +1,14 @@
+"""
+Elliot Greenlee
+Date Started: 2018-01-22
+University of Tennessee, Knoxville: WIND
+"""
+
+
+class Tracker:
+    def __init__(self, frame, targets):
+        # Initialize parallel trackers, one for each target
+        return
+
+    def track(self, frame, targets):
+        return targets
