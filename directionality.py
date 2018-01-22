dae67d7af9d9c29fdf9d88c88ec5224ce14b63dd
Elliot Greenlee <egreenle@vols.utk.edu>
2018-01-22 11:46:57 -0500

    add directionality computation stubs

---
+++ directionality.py
@@ -0,0 +1,15 @@
+"""
+Elliot Greenlee
+Date Started: 2018-01-22
+University of Tennessee, Knoxville: WIND
+"""
+
+
+def aggregator():
+    counts = 0
+    return counts
+
+
+def direction(counts):
+    angle = -55
+    return angle
